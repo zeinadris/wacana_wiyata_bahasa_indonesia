f1c9cc765074072bcfa90037e26c30ff5d35841d
Zein Adri <zeinadri@gmail.com>
2022-07-09 10:45:37 +0700

    deploy heroku

--- wacana_wiyata_bahasa_indonesia/settings.py
+++ wacana_wiyata_bahasa_indonesia/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 import os
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,8 +127,12 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Mengaktifkan Django-Heroku.
+django_heroku.settings(locals())
